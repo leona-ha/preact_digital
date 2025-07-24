c91c3b3bf661bb427cb69041d1d34c47f2353999
Meyer <tessa.christin.meyer@googlemail.com>
2025-07-24 16:08:13 +0200

    add er_aggregation FOR ER analyses, server_config.py and .here file for relative paths

---
+++ code/src/server_config.py
@@ -0,0 +1,10 @@
+# configuration file for the TIKI project
+
+data_path = "/sc-projects/sc-proj-cc15-preact/SP6/tiki_data"
+preprocessed_path = "/sc-projects/sc-proj-cc15-preact/SP6/preprocessed/preprocessed_before_2307"
+raw_path = "/sc-projects/sc-proj-cc15-preact/SP6/tiki_data/raw"
+redcap_path = "/sc-projects/sc-proj-cc15-preact/SP6/tiki_data/redcap/"
+#preprocessed_path_freezed = "/sc-projects/sc-proj-cc15-preact/SP6/tiki_data/preprocessed_freezed"
+#credential_path = "/home/leha18/tiki_code/notebooks/tiki-399609-b68b5ade4dfb.json"
+#proj_sheet = "1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
+#drivesheet_url = "https://docs.google.com/spreadsheets/d/1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
