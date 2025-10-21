0a857095ad774d35621d5085fed975b020d21878
Michał Łukomski <michal.lukomski21@gmail.com>
2025-10-21 12:41:55 +0200

    del __pycache__ and .ipynb checkpoints

--- .ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# configuration file for the TIKI project
-
-datapath = "/Volumes/Leonas_SSD/tiki_data"
-proj_sheet = "1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
-credential_path = "/Users/leonahammelrath/Documents/GitHub/tiki_code/notebooks/tiki-399609-b68b5ade4dfb.json"
-preprocessed_path = "/Volumes/Leonas_SSD/tiki_data/preprocessed"
-raw_path = "/Volumes/Leonas_SSD/tiki_data/raw"
-redcap_path = "/Volumes/Leonas_SSD/tiki_data/redcap"
-drivesheet_url = "https://docs.google.com/spreadsheets/d/1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
